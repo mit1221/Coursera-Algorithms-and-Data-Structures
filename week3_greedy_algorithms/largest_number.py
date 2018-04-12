188b7d3d111c1792807f8dbff2d57fc865f45523
Mit <mit_rupang@yahoo.ca>
2018-04-12 13:35:44 -0400

    Added files

--- week3_greedy_algorithms/largest_number.py
+++ week3_greedy_algorithms/largest_number.py
@@ -15,25 +15,13 @@ def largest_number(a):
     return answer
 
 
-def isGreaterOrEqual(num1, num2) -> bool:
-    """Return True if num1 is greater than or equal to num2"""
-    num1_list = [num1[i] for i in range(len(num1))]
-    num2_list = [num2[i] for i in range(len(num2))]
-    i = 0
-    j = 0
-    counter = 0
-    while counter != max(len(num1_list), len(num2_list)):
-        if num1_list[i] > num2_list[j]:
-            return True
-        elif num1_list[i] < num2_list[j]:
-            return False
-        else:
-            if i + 1 <= len(num1_list) - 1:
-                i += 1
-            if j + 1 <= len(num2_list) - 1:
-                j += 1
-        counter += 1
-    return True
+def isGreaterOrEqual(a, b) -> bool:
+    str1 = str(a) + str(b)
+    str2 = str(b) + str(a)
+    if int(str1) < int(str2):
+        return False
+    else:
+        return True
 
 
 if __name__ == '__main__':
